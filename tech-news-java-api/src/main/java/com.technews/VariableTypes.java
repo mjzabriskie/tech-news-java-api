58d7ede69846f46158935a3cbf1135429d42681f
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2023-02-20 23:38:44 -0700

    Correcting project file structure

---
+++ tech-news-java-api/src/main/java/com.technews/VariableTypes.java
@@ -0,0 +1,7 @@
+package src.main.java.com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ tech-news-java-api/src/main/java/com.technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package src.main.java.com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 0.55;
+        double sum = x+y;
+
+        String added = "hello!";
+        String result = added+x;
+
+        System.out.println(sum);
+        System.out.println(result);
+    }
+}
